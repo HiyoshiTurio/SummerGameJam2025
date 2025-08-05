735b7144a6d03c9b27caf595f58a7844018ae2ee
shuto20051014 <shuto20051014@gmail.com>
2025-08-05 14:37:35 +0900

    [fix]スタートボタンのアニメーション

--- Assets/Ito/Blink.cs
+++ Assets/Ito/Blink.cs
@@ -5,22 +5,26 @@ public class Blink : MonoBehaviour
 {
     [SerializeField] GameObject _gameObject;
     [Header("“_–Å‚³‚¹‚½‚¢Image"), SerializeField] Image targetImage;
-    [SerializeField] float blinkSpeed = 2f;
+    [SerializeField] float blinkSpeed = 1f;
     public bool isSelected = true;
+    private float alpha;
+    private float timer = Mathf.PI/2;
 
     void Update()
     {
         Vector3 mousePos = Input.mousePosition;
-        _gameObject.transform.position = mousePos;
+        _gameObject.transform.position = mousePos;   
         if (!isSelected)
         {
-            float alpha = (Mathf.Sin(Time.time * blinkSpeed) + 1f) / 2f;
+            alpha = (Mathf.Sin(timer * blinkSpeed) + 1f) / 2f;
             targetImage.color = new Color(1, 1, 1, alpha);
         }
         else
         {
             targetImage.color = new Color(1, 1, 1, 1);
+            timer = Mathf.PI/2;
         }
+        timer += 0.01f;
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
