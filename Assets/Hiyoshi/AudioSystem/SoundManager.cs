e7d20d051072853045f16cd5206b145f79ea7996
HiyoshiTurio <134475619+HiyoshiTurio@users.noreply.github.com>
2025-08-05 11:01:55 +0900

    [add] AudioManagerの追加

---
+++ Assets/Hiyoshi/AudioSystem/SoundManager.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+    public static SoundManager Instance => _instance;
+    
+    public List<AudioClip> audioClips;
+    private AudioSource[] _audioSources = new AudioSource[16];
+    public AudioClip bgmAudioClip;
+    private AudioSource _bgmAudioSource;
+    private AudioSource _drumRollAudioSource;
+    private int _playBackCount = 0;
+
+    private void Awake()
+    {
+        if (_instance == null) _instance = this;
+        else Destroy(this);
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            _audioSources[i] = gameObject.AddComponent<AudioSource>();
+        }
+
+        _bgmAudioSource = gameObject.AddComponent<AudioSource>();
+        _drumRollAudioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void Play(SoundKey soundKey)
+    {
+        _audioSources[_playBackCount].clip = audioClips[(int)soundKey];
+        _audioSources[_playBackCount].Play();
+        _playBackCount = (_playBackCount++) % 16;
+    }
+
+    public void PlayBgm()
+    {
+        _bgmAudioSource.clip = bgmAudioClip;
+        _bgmAudioSource.loop = true;
+        _bgmAudioSource.Play();
+    }
+    public void StopBgm()
+    {
+        _bgmAudioSource.Stop();
+    }
+
+    public void PlayDrumRoll()
+    {
+        _drumRollAudioSource.clip = audioClips[(int)SoundKey.DrumRoll];
+        _drumRollAudioSource.loop = true;
+        _drumRollAudioSource.Play();
+    }
+    public void StopDrumRoll()
+    {
+        _drumRollAudioSource.clip = audioClips[(int)SoundKey.DrumRollEnd];
+        _drumRollAudioSource.loop = false;
+        _drumRollAudioSource.Play();
+    }
+}
+
+public enum SoundKey
+{
+    ClapHunds,
+    DrumRoll,
+    DrumRollEnd,
+    Okawari,
+    PlayKoto,
+    PutGruss,
+    RestockSoba,
+    ScoreDown,
+    ScoreUp,
+    ShowResult,
+    StartCountDown,
+    TimeUp,
+    Title,
+    FootStep,
+}
